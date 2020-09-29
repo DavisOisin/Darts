9b819ed0bb0bf984ae2cd190c76ca28c7b1f84f3
DavisOisin <71603514+DavisOisin@users.noreply.github.com>
2020-09-29 16:05:17 +0100

    revamped realhistory for undo dart

--- scripts/script.js
+++ scripts/script.js
@@ -37,31 +37,31 @@ function startGame()
    document.getElementById("p2name").innerHTML = player2.name;
    document.getElementById("p1score").innerHTML = startingScore;
    document.getElementById("p2score").innerHTML = startingScore;
+   document.getElementById("currentturn").innerHTML = player1.name + "'s turn";
+   document.getElementById("dart").innerHTML = "Dart " + currentDart;
 }
 
 
 
 function throwdart(points){
-   document.getElementById("notice").innerHTML = "";
+   document.getElementById("notice").innerHTML = "notice";
 
    if (player1.turn)
    {
       updateScore(player1, points)
       checkwin(player1)
-      document.getElementById("p1history").innerHTML = player1.history;
    }
    else
    {
       updateScore(player2, points)
       checkwin(player2)
-      document.getElementById("p2history").innerHTML = player2.history;
    }
 
    document.getElementById("realhistory1").innerHTML = player1.realhistory;
    document.getElementById("realhistory2").innerHTML = player2.realhistory;
 
-   displayHistory(player1)
-   displayHistory(player2)
+   // displayHistory(player1)
+   // displayHistory(player2)
 
    document.getElementById("p1score").innerHTML = player1.score;
    document.getElementById("p2score").innerHTML = player2.score;
@@ -87,8 +87,9 @@ function throwdart(points){
 
 function updateScore(p, points)
 {
+   var prev = p.score;
    p.score -= points;
-   p.realhistory.push(document.myform.stage.value + ":" + currentDart + ":" + p.score);
+   p.realhistory.push(prev + ":" + document.myform.stage.value + ":" + currentDart + ":" + p.score);
    p.history.push(points)
 }
 
@@ -98,14 +99,13 @@ function checkwin(p)
    {
       document.getElementById("victory").style.display = "inline";
       document.getElementById("winner").innerHTML = p.name + " wins!";
-      p.realhistory.push("win");
       p.history.push("< WIN")
       document.getElementById("theBoard").style.display = "none";
    }
    else if (p.score <= 1)
    {
       document.getElementById("notice").innerHTML = p.name + " is bust!";
-      p.realhistory.push("bust:" + currentDart + ":" + p.score);
+      //p.realhistory.push("bust:" + currentDart + ":" + p.score);
 
       var addback = 0;
       var last;
@@ -154,30 +154,43 @@ function displayHistory(p)
 
 function undoLast()
 {
-    if (currentDart == 1)
-    {
-        currentDart = 3;
-        player1.turn = !player1.turn;
-        player2.turn = !player2.turn;
-    }
-    else
-    {
-        currentDart -= 1;
-    }
+   if (player1.realhistory.length != 0 || player2.realhistory.length != 0)
+   {
+      if (currentDart == 1)
+      {
+         player1.turn = !player1.turn;
+         player2.turn = !player2.turn;
+      }
 
-    var x;
-    if (player1.turn)
-    {
-        x = player1.realhistory.pop().split(":");
-        player1.score += Number(x[2])
+      var x;
+      if (player1.turn)
+      {
+         x = player1.realhistory.pop().split(":");
+         player1.score = Number(x[0])
+         currentDart = Number(x[2])
+         document.getElementById("p1score").innerHTML = player1.score;
+         document.getElementById("currentturn").innerHTML = player1.name + "'s turn";
+         document.getElementById("realhistory1").innerHTML = player1.realhistory;
 
-    }
-    else
-    {
-        x = player2.realhistory.pop().split(":");
-        player2.score += Number(x[2])
-    }
-    document.getElementById("currentturn").innerHTML = player1.name + "'s turn";
+      }
+      else
+      {
+         x = player2.realhistory.pop().split(":");
+         player2.score = Number(x[0])
+         currentDart = Number(x[2])
+         document.getElementById("p2score").innerHTML = player2.score;
+         document.getElementById("currentturn").innerHTML = player2.name + "'s turn";
+         document.getElementById("realhistory2").innerHTML = player2.realhistory;
+      }
+      document.getElementById("dart").innerHTML = "Dart " + currentDart;
+      document.getElementById("notice").innerHTML = "Dart Undone!";
+   }
+   else
+   {
+      document.getElementById("notice").innerHTML = "No darts to undo!";
+   }
+   document.getElementById("victory").style.display = "none";
+   document.getElementById("theBoard").style.display = "inline";
 }
 
 
